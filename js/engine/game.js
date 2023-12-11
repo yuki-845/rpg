a430e30ecad826da39a929cb7d7085a10a5472d9
yicode <120433484+yuki-845@users.noreply.github.com>
2023-12-11 13:38:48 +0900

    コミット

--- js/engine/game.js
+++ js/engine/game.js
@@ -19,16 +19,68 @@ class Game {
 		this.canvas.width = width || 320;
 		//canvasの縦幅（ゲームの縦幅）を設定。もし縦幅が指定されていなければ320を代入
 		this.canvas.height = height || 320;
-        //ゲームに登場する全てのもの（オブジェクト）を入れるための配列
-		this.objs = [];
+
+		//シーンを入れておくための配列
+		this.scenes = [];
+		//現在のシーンをいれておくためのもの
+		this.currentScene;
+
+		//ゲームに使用するキーと、そのキーが押されているかどうかを入れるための連想配列
+		//例 { up: false, down: false }
+		this.input = {};
+		//登録されたキーに割り当てられたプロパティ名と、キー名を、関連づけるための連想配列
+		//例 { up: "ArrowUp", down: "ArrowDown" }
+		this._keys = {};
 	} //constructor() 終了
-    /**
+
+	/**
 	 * startメソッドを呼び出すことで、メインループが開始される
 	 */
 	start() {
+		//デフォルトのキーバインドを登録する（使いたいキーを登録する）
+		this.keybind( 'up', 'ArrowUp' );
+		this.keybind( 'down', 'ArrowDown' );
+		this.keybind( 'right', 'ArrowRight' );
+		this.keybind( 'left', 'ArrowLeft' );
+
+		//現在のシーン（currentScene）になにも入っていないときは、scenes[0]を代入
+		this.currentScene = this.currentScene || this.scenes[0];
+
 		//メインループを呼び出す
 		this._mainLoop();
+
+		//イベントリスナーをセットする
+		this._setEventListener();
 	} //start() 終了
+
+	/**
+	 * イベントリスナーをセットするためのメソッド
+	 */
+	_setEventListener() {
+		//なにかキーが押されたときと、はなされたときに呼ばれる
+		const _keyEvent = e => {
+			//デフォルトのイベントを発生させない
+			e.preventDefault();
+			//_keysに登録された数だけ繰り返す
+			for ( let key in this._keys ) {
+				//イベントのタイプによって呼び出すメソッドを変える
+				switch ( e.type ) {
+					case 'keydown' :
+						//押されたキーが、登録されたキーの中に存在するとき、inputのそのキーをtrueにする
+						if ( e.key === this._keys[key] ) this.input[key] = true;
+						break;
+					case 'keyup' :
+						//押されたキーが、登録されたキーの中に存在するとき、inputのそのキーをfalseにする
+						if ( e.key === this._keys[key] ) this.input[key] = false;
+						break;
+				}
+			}
+		}
+		//なにかキーが押されたとき
+		addEventListener( 'keydown', _keyEvent, { passive: false } );
+		//キーがはなされたとき
+		addEventListener( 'keyup', _keyEvent, { passive: false } );
+	} //_setEventListener() 終了
 
 	/**
 	 * メインループ
@@ -41,24 +93,44 @@ class Game {
 		//左上から、画面のサイズまでを、塗りつぶす
 		ctx.fillRect( 0, 0, this.canvas.width, this.canvas.height );
 
-		//自分自身（_mainLoop）を呼び出して、ループさせる
-		requestAnimationFrame( this._mainLoop.bind( this ) );
-        //ゲームに登場する全てのもの（オブジェクト）の数だけ繰り返す
-		for ( let i=0; i<this.objs.length; i++ ) {
-			//スプライトやテキストなど、すべてのオブジェクトのupdateメソッドを呼び出す
-			this.objs[i].update( this.canvas );
+		//現在のシーンのupdateメソッドを呼び出す
+		this.currentScene.update();
+
+		//現在のシーンの、ゲームに登場する全てのもの（オブジェクト）の数だけ繰り返す
+		for ( let i=0; i<this.currentScene.objs.length; i++ ) {
+			//現在のシーンの、すべてのオブジェクトのupdateメソッドを呼び出す
+			this.currentScene.objs[i].update( this.canvas );
 		}
 
+		//自分自身（_mainLoop）を呼び出して、ループさせる
+		requestAnimationFrame( this._mainLoop.bind( this ) );
 	} //_mainLoop() 終了
-    /**
-	 * オブジェクトをゲームに追加できるようになる、addメソッドを作成
+
+	/**
+	 * ゲームにシーンに追加できるようになる、addメソッドを作成
 	 *
 	 * 引数
-	 * obj : 追加したいオブジェクト
+	 * scene : 追加したいシーン
 	 */
-	add( obj ) {
-		//this.objs配列の末尾に、objの値を追加
-		this.objs.push( obj );
-	} //add() 終了
+	add( scene ) {
+		//引数がSceneのとき、this.scenesの末尾にsceneを追加
+		if ( scene instanceof Scene ) this.scenes.push( scene );
+		//引数がSceneでなければ、コンソールにエラーを表示
+		else console.error( 'Gameに追加できるのはSceneだけだよ！' );
+	} //add()終了
+
+	/**
+	 * 使いたいキーを登録できるようになる、keybindメソッドを作成
+	 *
+	 * 引数
+	 * name : キーにつける名前
+	 * key : キーコード
+	 */
+	keybind( name, key ) {
+		//キーの名前と、キーコードを関連づける
+		this._keys[name] = key;
+		//キーが押されているかどうかを入れておく変数に、まずはfalseを代入しておく
+		this.input[name] = false;
+	} //keybind() 終了
 
 }
--- js/engine/sprite.js
+++ js/engine/sprite.js
@@ -6,16 +6,23 @@
 class Sprite {
 
 	/**
-	 * 引数
-	 * img : 画像ファイルまでのパス
+	 * 引数img : 画像ファイルまでのパス
+	 * width : 画像の表示する範囲（横幅）
+	 * height : 画像の表示する範囲（縦幅）
 	 */
-	constructor( img ) {
+	constructor( img, width, height ) {
 		//this.imgに、あなたは画像ですよ、と教える
 		this.img = new Image();
 		//this.img.srcに画像ファイルまでのパスを代入
 		this.img.src = img;
 		//画像の初期位置
 		this.x = this.y = 0;
+		//画像を表示する範囲の横幅。引数widthが指定されていない場合、this.widthに32を代入
+		this.width = width || 32;
+		//画像を表示する範囲の縦幅。引数heightが指定されていない場合、this.heightに32を代入
+		this.height = height || 32;
+		//何番目の画像を表示するか
+		this.frame = 0;
 	} //constructor() 終了
 
 	/**Gameクラスのメインループからずっと呼び出され続ける
@@ -26,6 +33,8 @@ class Sprite {
 	update( canvas ) {
 		//画像などを画面に表示するためのメソッドを呼び出す
 		this.render( canvas );
+		//スプライトを動かしたり、なにかのきっかけでイベントを発生させたりするために使うメソッドを呼び出す
+		this.onenterframe();
 	} //update() 終了
 
 	/**
@@ -35,10 +44,33 @@ class Sprite {
 	 * canvas : 紙（キャンバス）
 	 */
 	render( canvas ) {
+		//キャンバスの外にスプライトがあるとき、ここでこのメソッドを終了する
+		if ( this.x < -1 * this.width || this.x > canvas.width ) return;
+		if ( this.y < -1 * this.height || this.y > canvas.height ) return;
+
+		//X,Y方向に、何番目の画像か
+		const _frameX = this.frame % ( this.img.width / this.width );
+		const _frameY = ~~( this.frame / ( this.img.width / this.width ) );
+
 		//画家さん（コンテキスト）を呼ぶ
 		const _ctx = canvas.getContext( '2d' );
 		//画家さんに、絵を描いてとお願いする
-		_ctx.drawImage( this.img, this.x, this.y );
+		_ctx.drawImage(
+			this.img,
+			this.width * _frameX,
+			this.height * _frameY,
+			this.width,
+			this.height,
+			this.x,
+			this.y,
+			this.width,
+			this.height
+		);
 	} //render() 終了
+
+	/**
+	 * 常に呼び出され、スプライトの移動やイベントの発生などに使うメソッド。空なのはオーバーライド（上書き）して使うため
+	 */
+	onenterframe() {}
 
 }
