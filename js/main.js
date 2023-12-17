4dd2e22941dab77341fcf1b45e0f876d1ddb963f
yicode <120433484+yuki-845@users.noreply.github.com>
2023-12-17 19:53:20 +0900

    comit

--- js/main.js
+++ js/main.js
@@ -1,32 +1,53 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Canvas要素を取得
+    var canvas = document.getElementById('myCanvas');
+    var ctx = canvas.getContext('2d');
 
-var objs = [];
-var x = 0;
-var y = 0;
-function mainLoop() {
-    const canvas = document.createElement("canvas");
-document.body.appendChild(canvas);
-const ctx = canvas.getContext("2d");
-    var myImage = new Image();
-    myImage.src = "img/aru.png";
-    const frameX = 0 % (myImage.width / 32 );
-	const frameY = ~~( 0 / ( myImage.width / 32 ) );
-    
-    myImage.onload = () => {
-        ctx.drawImage(
-			myImage,
-			32 * frameX,
-			32 * frameY,
-			32,
-			32,
-			x,
-			y,
-			32,
-			32
-		);
+    // 画像のパスのリスト
+    const map = [
+		[11,11,11,11,11,11,11,11,11,11],
+		[11,10,10,10,10,10,10,10,10,11],
+		[11, 4, 4, 4, 4, 4, 4, 4, 4,11],
+		[11, 4,11, 4, 4,11,11,11, 4,11],
+		[11, 4,11,11,11,11,10,10, 4,11],
+		[11, 4,11,10,10,11, 4, 4, 4,11],
+		[11, 4,11, 4, 4,11,11,11, 4,11],
+		[11, 4, 9, 4, 4, 9,10,11, 4,11],
+		[11, 4, 4, 4, 4, 4, 4,11, 4,11],
+		[11,11,11,11,11,11,11,11,11,11]
+	];
+
+    // 画像を読み込むための関数
+    function loadImage(path, callback) {
+        var img = new Image();
+        img.onload = function () {
+            callback(img);
+        };
+        img.src = path;
     }
-    
-}
-addEventListener( 'load', () => {
-    
-	requestAnimationFrame(mainLoop());
-} );
+
+    // 画像を描画するための関数
+    function drawImageOnCanvas(image, x, y) {
+        ctx.drawImage(image, x, y, 32, 32); // 画像のサイズは適宜調整
+    }
+
+    // すべての画像を描画する
+    function drawAllImages() {
+        var x = 0;
+        var y = 0;
+
+        for (var i = 0; i < map.length; i++) {
+            loadImage("img/tile.png", function (image) {
+                //X方向に、何番目の画像か
+				const frameX = map[y][x] % ( image.width / 32 );
+				//Y方向に、何番目の画像か
+				const frameY = ~~( map[y][x] / ( image.width / 32 ) );
+                drawImageOnCanvas(image, x, y);
+                x += 120; // 画像の間隔を調整
+            });
+        }
+    }
+
+    // 画像の描画を開始
+    drawAllImages();
+});
