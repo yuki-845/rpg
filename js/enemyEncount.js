fca653d007aa9f2ab9902f3985c593d62ce9fca9
yicode <120433484+yuki-845@users.noreply.github.com>
2024-01-19 10:58:00 +0900

    色々変更

---
+++ js/enemyEncount.js
@@ -0,0 +1,16 @@
+function enemyEncount() {
+    
+    ctx.beginPath();
+    //背景
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.rect( 37, 284, 162, 123 );
+    ctx.strokeStyle = "white" ;
+    ctx.lineWidth = 2;
+    ctx.stroke() ;
+    ctx.rect( 210, 284, 403, 123 );
+    ctx.strokeStyle = "white" ;
+    ctx.lineWidth = 2;
+    ctx.stroke() ;
+
+}
